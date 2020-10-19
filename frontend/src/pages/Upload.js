411fbed3b1a70a07b582f40169c311013306dad1
Eric Mendez <ericyoary@gmail.com>
2020-10-18 18:59:14 -0700

    upload Image

---
+++ frontend/src/pages/Upload.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import Alert from '../components/Alert';
+
+import axios from 'axios'
+
+export default function Upload() {
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        previewFile(file);
+        setSelectedFile(file);
+        setFileInputState(e.target.value);
+    };
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
+        };
+    };
+
+    const handleSubmitFile = (e) => {
+        e.preventDefault();
+        if (!selectedFile) return;
+
+        uploadImage()
+    };
+
+
+    const uploadImage = async () => {
+        const data = new FormData()
+        console.log(selectedFile);
+        data.append('file', selectedFile)
+        data.append('upload_preset', 'tpgtfsjg')
+        try {
+
+
+            axios({
+                url: 'https://api.cloudinary.com/v1_1/ericyoary/upload',
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                data: data
+            }).then((res) => {
+                console.log(res);
+                let imageURL = res.data.url
+            })
+        } catch (err) {
+            console.error(err);
+            setErrMsg('Something went wrong!');
+        }
+    };
+    return (
+        <div>
+            <h1 className="title">Upload an Image</h1>
+            <Alert msg={errMsg} type="danger" />
+            <Alert msg={successMsg} type="success" />
+            <form onSubmit={handleSubmitFile} className="form">
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                    className="form-input"
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+            {previewSource && (
+                <img
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
+        </div>
+    );
+}
